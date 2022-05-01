94458d7097782cfa6cf034f0bd5d2c0844b8c2cb
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-05-01 16:29:05 -0500

    Added getImages.php, Completed template, and added async method to get images.

--- src/components/PhotoDisplay.vue
+++ src/components/PhotoDisplay.vue
@@ -1,12 +1,63 @@
 <template>
     <div>
-        <p>test</p>
+        <button type="submit">Add Image</button>
+        <h2>Welcome to your photography portfolio {{ username }}!</h2>
+        <h3 v-if="status == 'error'">{{ message }}</h3>
+        <div class="photoCard" v-for="image in images" :key="image.PortID">
+            <img v-bind:src="image.Picture">
+            <p>{{ image.Description }}</p>
+            <p>{{ image.DateTaken }}</p>
+            <p>{{ image.UploadDate }}</p>
+            <form class="modify">
+                <button type="submit">Update</button>
+                <button type="submit">Delete</button>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
+import store from "@/store/index.js";
+
 export default {
-    
+    computed: {
+        username: function() {
+            return store.state.username;
+        },
+        session: function() {
+            return store.state.session;
+        }
+    },
+    data: function() {
+        return {
+            images: [],
+            status: "",
+            message: ""
+        }
+    },
+    methods: {
+        getImages: async function() {
+            const url = "http://localhost/Client-Side_Programming/tylerkaufmannfinal/src/php/getImages.php";
+            var currentSession = this.session;
+
+            const response = await fetch(url, {
+                method: 'POST',
+                body: {
+                    session: currentSession
+                }
+            });
+
+            const data = await response.json();
+            
+            this.status = data.status;
+
+            if (data.status == "success") {
+                this.images = data.images;
+            } else {
+                this.message = data.message;
+            }
+        }
+    }
 }
 </script>
 
---
+++ src/php/getImages.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once "config.php";
+
+session_id($_POST['session']);
+session_start();
+
+if ($_SESSION['auth'])
+{
+    $connection = new mysqli($hn, $un, $pw, $db);
+
+    if ($connection->connect_error)
+    {
+        $response['status'] = "db error";
+        $response['message'] = "Failed to connect to database";
+    }
+    else
+    {
+        $id = $_SESSION['userID'];
+
+        $query = "SELECT PortID, Picture, DateTaken, Description, UploadDate FROM portfolio WHERE UserID=$id";
+        $result = $connection->query($query);
+
+        if ($result)
+        {
+            $rows = $result->num_rows;
+
+            if ($rows > 0)
+            {
+                $response['status'] = "success";
+                $images = array();
+                
+                for ($inex = 0; $index < $rows; $index++)
+                {
+                    $images[] = $result->fetch_row();
+                }
+
+                $response['images'] = $images;
+            }
+            else
+            {
+                $response['status'] = "error";
+                $response['message'] = "No images found. Click on the 'Add Image' button to add your first image!";
+            }
+        }
+        else
+        {
+            $response['status'] = "db error";
+            $response['message'] = "failed to get image data";
+        }
+
+        $result->close();
+    }
+
+    $connection->close();
+}
+else
+{
+    $response['status'] = "error";
+    $response['message'] = "Not authorized. Please log in and try again";
+}
+
+echo json_encode($response);
+
+?>
