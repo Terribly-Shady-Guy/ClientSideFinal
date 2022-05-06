5adc8a4cde878d8a0ad97514bcf69b8cf59ea194
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-05-06 18:42:10 -0500

    added html and styled about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,5 +2,24 @@
   <div class="about">
     <h1>Welcome to the Digital Photography Portfolio website!</h1>
     <p>This is a website for photographers to upload their favorite photos. To get started, create a new account.</p>
+    <h2>How to add a photo to the portfolio</h2>
+    <ol>
+      <li>Create an account</li>
+      <li>Log in to your new account</li>
+      <li>Click on "Insert Image" to add your first photo</li>
+      <li>Select file and fill in requested information</li>
+      <li>Click "add" to submit</li>
+    </ol>
   </div>
 </template>
+
+<style scoped>
+h2, p {
+  text-align: center;
+}
+
+ol {
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
